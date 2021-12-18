bdcb65db1f02e365d4dc5ea74a5bfe8ab9633735
Sangmin Kim <sangmin.kim@business-canvas.com>
2021-12-18 12:28:55 +0900

    git init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import os from 'os';
+import http from 'http'
+
+const handleRequest = function(request, response) {
+    response.writeHead(200);
+    response.end("Hello World! I'm "+os.hostname());
+    console.log("["+
+        Date(Date.now()).toLocaleString()+
+        "] "+os.hostname());
+}
+
+const www = http.createServer(handleRequest);
+
+www.listen(8080);
